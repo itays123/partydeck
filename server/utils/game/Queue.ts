14c1850fdcf7e65d376e22e068684b5af111244c
itays123 <itays2005@gmail.com>
2020-10-27 10:47:53 +0200

    setup Queue

---
+++ server/utils/game/Queue.ts
@@ -0,0 +1,31 @@
+import { LinkedList } from './LinkedList.ts';
+
+export class Queue<T = any> {
+  private linkedList: LinkedList<T>;
+
+  constructor() {
+    this.linkedList = new LinkedList();
+  }
+
+  isEmpty() {
+    return !this.linkedList.head;
+  }
+
+  peek() {
+    if (!this.linkedList.head) return null;
+    return this.linkedList.head.value;
+  }
+
+  enqueue(value: T) {
+    this.linkedList.append(value);
+  }
+
+  dequeue() {
+    const removedHead = this.linkedList.deleteHead();
+    return removedHead ? removedHead.value : null;
+  }
+
+  fromArray(array: T[]) {
+    this.linkedList.fromArray(array);
+  }
+}
