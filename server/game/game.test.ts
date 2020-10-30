7728a009c533da497710b260bc7bda073cbfa3dd
itays123 <itays2005@gmail.com>
2020-10-30 15:08:23 +0200

    REFACTOR: Deno unit testing

--- server/game/game.test.ts
+++ server/game/game.test.ts
@@ -1,4 +1,3 @@
-import { sleepRandomAmountOfSeconds } from '../deps.ts';
 import { IPlayer } from '../types.ts';
 import { Game } from './Game.ts';
 
@@ -12,55 +11,66 @@ export class TestPlayer implements IPlayer {
   }
 
   async boradcast(message: any): Promise<void> {
-    console.log('on:', this.nickname, 'message:', JSON.stringify(message));
+    // cool!
   }
 }
 
-const game = new Game<TestPlayer>([
+const QUESTIONS = [
   'question1',
   'question2',
   'question3',
   'question4',
   'question5',
   'question6',
-]);
+];
+const PLAYERS = [
+  new TestPlayer('player1'),
+  new TestPlayer('player2'),
+  new TestPlayer('player3'),
+];
 
-let rounds: any[];
+Deno.test('runs a game with 6 questions', async () => {
+  const game = new Game<TestPlayer>(QUESTIONS);
+  let rounds: any[] = [];
 
-game.on('start', async (players: Map<string, TestPlayer>) => {
-  console.log(players.size, 'players joined!');
-  rounds = [];
-});
-
-game.on('round', async (players: Map<string, TestPlayer>, judgeId: string) => {
-  await sleepRandomAmountOfSeconds(1, 5, false);
-  let fallbackPlayer: string;
-  for (const playerId of players.keys()) {
-    if (playerId !== judgeId) {
-      fallbackPlayer = playerId;
-      if (Math.random() < 0.2) {
-        rounds.push({
-          judge: players.get(judgeId)!.nickname,
-          winner: players.get(playerId)!.nickname,
-        });
-        return playerId;
-      }
-    }
-  }
-  rounds.push({
-    judge: players.get(judgeId)!.nickname,
-    winner: players.get(fallbackPlayer!)!.nickname,
+  game.on('start', async (players: Map<string, TestPlayer>) => {
+    if (players.size !== 3) throw new Error('there are not three players');
   });
-  return fallbackPlayer!;
+
+  game.on(
+    'round',
+    async (players: Map<string, TestPlayer>, judgeId: string) => {
+      let fallbackPlayer: string;
+      for (const playerId of players.keys()) {
+        if (playerId !== judgeId) {
+          fallbackPlayer = playerId;
+          if (Math.random() < 0.2) {
+            rounds.push({
+              judge: players.get(judgeId)!.nickname,
+              winner: players.get(playerId)!.nickname,
+            });
+            return playerId;
+          }
+        }
+      }
+      rounds.push({
+        judge: players.get(judgeId)!.nickname,
+        winner: players.get(fallbackPlayer!)!.nickname,
+      });
+      return fallbackPlayer!;
+    }
+  );
+
+  game.on('end', async () => {
+    console.table(rounds);
+  });
+
+  for (const player of PLAYERS) {
+    game.addPlayer(player);
+  }
+  await game.start();
+
+  if (rounds.length !== 6) {
+    throw new Error('wrong number of rounds');
+  }
 });
-
-game.on('end', async () => {
-  console.log('game ended');
-  console.table(rounds);
-});
-
-game.addPlayer(new TestPlayer('player1'));
-game.addPlayer(new TestPlayer('player2'));
-game.addPlayer(new TestPlayer('player3'));
-
-game.start();
