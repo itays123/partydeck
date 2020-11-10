a2421f9b5c0ce308f12db9b3161209137c3fdf91
itays123 <itays2005@gmail.com>
2020-11-10 15:14:36 +0200

    REFACTOR: server tests

--- server/server.test.ts
+++ server/server.test.ts
@@ -3,20 +3,16 @@ import { Player } from './player/Player.ts';
 import { Server } from './server.ts';
 import { assertEquals } from './deps.ts';
 
-Deno.test('connects 3 players to the same game', async () => {
+Deno.test('runs a game', async () => {
   assertEquals(
     await Server.test(
       (
         pending: Map<string, Game<Player>>,
         active: Map<string, Game<Player>>
       ) => {
-        for (const game of pending.values()) {
-          console.log(game.playerCount);
-          if (game.playerCount === 3) return false;
-        }
         return pending.size > 0 || active.size > 0;
       }
     ),
-    3
+    4 // another request for stopping the server later
   );
 });
