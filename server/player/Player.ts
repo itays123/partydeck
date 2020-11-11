c90d60c3368ec7c098a6a5c23f325f0332981089
itays123 <itays2005@gmail.com>
2020-11-11 15:03:48 +0200

    FIX: pick the first card if judge disconnected

--- server/player/Player.ts
+++ server/player/Player.ts
@@ -23,6 +23,7 @@ export class Player extends BasePlayer {
   static async roundHandler(cards: PickedCard[], judge: Player) {
     console.log('round started', cards);
     while (judge.pickedCard === null) {
+      if (!judge.isConnected) return cards[0].playerId;
       const timeout = Timeout.wait(1000);
       await timeout;
     }
