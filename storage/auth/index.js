adf6be25ea2fd0ab477422c6cc97bd6565b990d5
itays123 <itays2005@gmail.com>
2020-11-15 14:05:41 +0200

    REFACTOR: remove console.logs

--- storage/auth/index.js
+++ storage/auth/index.js
@@ -15,7 +15,6 @@ router.post('/login', async (req, res) => {
 
 router.post('/register', async (req, res) => {
   try {
-    console.log('handling request...', req.body);
     const { email, password, name = 'anonymous' } = req.body;
     const token = await User.register(email, password, name);
     res.status(201).json({ token });
