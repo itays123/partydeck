bc44c344bc87cb35183d1ad5c53f5e39b91b6655
itays123 <itays2005@gmail.com>
2020-12-01 15:32:35 +0200

    FIX: use a bundle.js file in the server microservice (#26)

--- server/server.ts
+++ server/server.ts
@@ -52,6 +52,7 @@ export class Server {
 
   static async serve(port: number) {
     const server = new Server();
+    console.log('server is up');
     for await (const req of serve({ port })) {
       const res = await server.handler(req);
       if (res) req.respond(res);
