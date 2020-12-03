7f7babf7863d30c96cc741ca855258d2dac5f18a
itays123 <itays2005@gmail.com>
2020-12-03 15:33:22 +0200

    REFACTOR: remove unused test game

--- server/server.ts
+++ server/server.ts
@@ -5,51 +5,6 @@ import { generate } from './shared/NumericID.ts';
 import { Acceptable, TestFunction } from './types.ts';
 
 export class Server {
-  static get TEST_GAME(): [string[], string[]] {
-    return [
-      ['question1', 'question2', 'question3'],
-      [
-        'a1',
-        'a2',
-        'a3',
-        'a4',
-        'a5',
-        'a6',
-        'a7',
-        'a8',
-        'a9',
-        'a10',
-        'a11',
-        'a12',
-        'a13',
-        'a14',
-        'a15',
-        'a16',
-        'a17',
-        'a18',
-        'a19',
-        'a20',
-        'a21',
-        'a22',
-        'a23',
-        'a24',
-        'a25',
-        'a26',
-        'a27',
-        'a28',
-        'a29',
-        'a30',
-        'a31',
-        'a32',
-        'a33',
-        'a34',
-        'a35',
-        'a36',
-        'a37',
-      ],
-    ];
-  }
-
   static async serve(port: number) {
     const server = new Server();
     console.log('server is up');
