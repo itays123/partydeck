f4fb561b6a858ca52f427643459ce2669146cb6a
itays123 <itays2005@gmail.com>
2020-12-06 20:35:01 +0200

    DOCKER: remove JWT_SECRET security risk

--- storage/shared/consts.js
+++ storage/shared/consts.js
@@ -3,6 +3,6 @@ module.exports.MONGO_URI =
   process.env.MONGO_URI || 'mongodb://localhost:27017/partydecks';
 module.exports.LIMIT_GAMES = process.env.LIMIT_GAMES || true;
 module.exports.PORT = process.env.PORT || 4000;
-module.exports.JWT_SECRET = process.env.JWT_SECRET || 'shh';
+module.exports.JWT_SECRET = process.env.JWT_SECRET;
 module.exports.SECURE = process.env.SECURE || false;
 module.exports.SERVER_URL = process.env.SERVER_URL || 'http://localhost:8000';
