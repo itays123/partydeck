74d1ff19db3eafcd0f4cc6f2749d4fbeb4a5dd12
itays123 <itays2005@gmail.com>
2020-12-06 20:30:19 +0200

    DOCKER: init storage

--- storage/shared/consts.js
+++ storage/shared/consts.js
@@ -1,7 +1,8 @@
 module.exports.HEADER = 'Authorization';
-module.exports.MONGO_URI = 'mongodb://localhost:27017/partydecks';
-module.exports.LIMIT_GAMES = true;
-module.exports.PORT = 4000;
-module.exports.JWT_SECRET = 'shh';
-module.exports.SECURE = false;
-module.exports.SERVER_URL = 'http://localhost:8000';
+module.exports.MONGO_URI =
+  process.env.MONGO_URI || 'mongodb://localhost:27017/partydecks';
+module.exports.LIMIT_GAMES = process.env.LIMIT_GAMES || true;
+module.exports.PORT = process.env.PORT || 4000;
+module.exports.JWT_SECRET = process.env.JWT_SECRET || 'shh';
+module.exports.SECURE = process.env.SECURE || false;
+module.exports.SERVER_URL = process.env.SERVER_URL || 'http://localhost:8000';
