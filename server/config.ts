f0f272b8b5b65abe86b5bf59e9cd4f642d5a62af
itays123 <itays2005@gmail.com>
2020-12-10 16:03:47 +0200

    FIX: critical environement issues

--- server/config.ts
+++ server/config.ts
@@ -1,2 +1,2 @@
 export const PORT = Number(Deno.env.get('PORT') || 8000);
-export const CONSOLE = Boolean(Deno.env.get('CONSOLE') || true);
+export const CONSOLE = Boolean(Deno.env.get('CONSOLE') !== 'false');
--- storage/shared/consts.js
+++ storage/shared/consts.js
@@ -1,8 +1,8 @@
 module.exports.HEADER = 'Authorization';
 module.exports.MONGO_URI =
   process.env.MONGO_URI || 'mongodb://localhost:27017/partydecks';
-module.exports.LIMIT_GAMES = process.env.LIMIT_GAMES || true;
-module.exports.PORT = process.env.PORT || 4000;
+module.exports.LIMIT_GAMES = Boolean(process.env.LIMIT_GAMES !== 'false');
+module.exports.PORT = Number(process.env.PORT || 4000);
 module.exports.JWT_SECRET = process.env.JWT_SECRET;
-module.exports.SECURE = process.env.SECURE || false;
+module.exports.SECURE = Boolean(process.env.SECURE === 'true');
 module.exports.SERVER_URL = process.env.SERVER_URL || 'http://localhost:8000';
