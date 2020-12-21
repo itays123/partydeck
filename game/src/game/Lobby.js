a98bf30e42566db17bf2ce8669b2fa1b4dbb7f4f
itays123 <itays2005@gmail.com>
2020-12-21 14:19:04 +0200

    FIX: responsive web design

--- game/src/game/Lobby.js
+++ game/src/game/Lobby.js
@@ -8,7 +8,7 @@ const Lobby = ({ gameCode, playerCount, isAdmin }) => {
         {gameCode.split('').map((char, i) => (
           <div
             key={i}
-            className="text-9xl bg-gray-100 text-gray-800 mx-2 rounded px-4 pb-4"
+            className="text-3xl md:text-6xl lg:text-9xl bg-gray-100 text-gray-800 mx-1 md:mx-2 rounded px-2 md:px-4 pb-4"
           >
             {char}
           </div>
