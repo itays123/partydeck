119167b1f5b4b7cae802f02b1083dcfdad3ce321
itays123 <itays2005@gmail.com>
2020-12-21 10:39:10 +0200

    FIX: Added key

--- game/src/game/Lobby.js
+++ game/src/game/Lobby.js
@@ -5,8 +5,11 @@ const Lobby = ({ gameCode, playerCount, isAdmin }) => {
   return (
     <div className="lobby container mx-auto">
       <div className="gamecode-display flex justify-center mt-4">
-        {gameCode.split('').map(char => (
-          <div className="text-9xl bg-gray-100 text-gray-800 mx-2 rounded px-4 pb-4">
+        {gameCode.split('').map((char, i) => (
+          <div
+            key={i}
+            className="text-9xl bg-gray-100 text-gray-800 mx-2 rounded px-4 pb-4"
+          >
             {char}
           </div>
         ))}
