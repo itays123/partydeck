6e4093652dd6d40d3c2926647d8675fccd005374
itays123 <itays2005@gmail.com>
2020-12-23 08:24:58 +0200

    FIX: replace navbar icon

--- game/src/navigation/Navbar.js
+++ game/src/navigation/Navbar.js
@@ -1,9 +1,12 @@
+import logo from '../partydeck.svg';
+
 const Navbar = () => {
   return (
     <div className="nav px-8 w-screen flex">
       <nav className="flex items-center h-16 flex-grow border-b border-purple-700">
         <div className="flex-grow px-6">
-          <h1 className="text-xl text-gray-200 font-bold">🎉Partydeck</h1>
+          <img src={logo} width={16} height={16} alt="" />
+          <h1 className="text-xl text-gray-200 font-bold">Partydeck</h1>
         </div>
       </nav>
     </div>
