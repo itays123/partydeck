b2943dc0a128475ad4bfaa65944ca76aef4b1f6b
itays123 <itays2005@gmail.com>
2020-12-24 16:40:51 +0200

    FIX: logo size

--- game/src/navigation/Navbar.js
+++ game/src/navigation/Navbar.js
@@ -4,8 +4,8 @@ const Navbar = () => {
   return (
     <div className="nav px-8 w-screen flex">
       <nav className="flex items-center h-16 flex-grow border-b border-purple-700">
-        <div className="flex-grow px-6">
-          <img src={logo} width={16} height={16} alt="" />
+        <div className="flex-grow px-6 flex">
+          <img src={logo} width={20} height={20} alt="" />
           <h1 className="text-xl text-gray-200 font-bold">Partydeck</h1>
         </div>
       </nav>
