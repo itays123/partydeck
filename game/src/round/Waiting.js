dd9d7240677426af7f769eaabb10685131320fa8
itays123 <itays2005@gmail.com>
2021-01-07 15:30:15 +0200

    REFACTOR: Spinner component

--- game/src/round/Waiting.js
+++ game/src/round/Waiting.js
@@ -1,5 +1,12 @@
+import Spinner from '../shared/Spinner';
+
 const Waiting = ({ text }) => {
-  return <div className="waiting">{text}</div>;
+  return (
+    <div className="waiting flex items-center justify-center mt-4 text-gray-100">
+      <Spinner />
+      <span>{text}</span>
+    </div>
+  );
 };
 
 export default Waiting;
