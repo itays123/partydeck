732d67cd652c45b6524641372c4027cb96e8d809
itays123 <itays2005@gmail.com>
2021-02-23 08:53:21 +0200

    FEATURE: Profile route for panel profile page

--- storage/auth/index.js
+++ storage/auth/index.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const { SECURE } = require('../shared/consts');
+const onlyAuth = require('./only-auth');
 const User = require('./user');
+const Game = require('../game/game');
 
 const router = express.Router();
+
+router.get('/profile', onlyAuth, async (req, res) => {
+  try {
+    const userId = req.uid;
+    const user = await User.getUserData(userId);
+    if (!user) throw new Error();
+    const games = await Game.getUserGames(userId, userId);
+    res.status(200).json({ user: { _id: userId, name: user.name, games } });
+  } catch (err) {
+    res.status(404).json({ err: 'User not found', code: 404 });
+  }
+});
 
 router.post('/login', async (req, res) => {
   try {
