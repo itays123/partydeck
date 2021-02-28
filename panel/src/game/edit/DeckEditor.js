427b5d62db84d7f92c1f34802bf4ec4c8b895173
itays123 <itays2005@gmail.com>
2021-02-28 13:33:21 +0200

    FEATURE: Edit existing & new cards

--- panel/src/game/edit/DeckEditor.js
+++ panel/src/game/edit/DeckEditor.js
@@ -6,12 +6,18 @@ import EditorOnly from './EditorOnly';
  * @param {{ editor: { deck: Map<number,string>, addCard(value:string): void, modifyCard(key: number, value:string): void, deleteCard(key): void } }} props
  */
 const DeckEditor = ({ editor }) => {
-  const { deck, addCard } = editor;
+  const { deck, addCard, modifyCard } = editor;
   return (
     <section className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-5 gap-4 mt-2 pb-6">
       {[...deck.keys()].map((key, index) => {
         const text = deck.get(key);
-        return <EditableCard key={key} text={text} onTextChange={() => {}} />;
+        return (
+          <EditableCard
+            key={key}
+            text={text}
+            onTextChange={value => modifyCard(key, value)}
+          />
+        );
       })}
       <EditorOnly>
         <button
