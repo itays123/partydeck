d1736e6ca9a436513ecac98c42fd71e70655b37e
itays123 <itays2005@gmail.com>
2021-03-02 13:22:24 +0200

    FEATURE: Authonly component

---
+++ panel/src/auth/AuthOnly.js
@@ -0,0 +1,12 @@
+import { Redirect } from 'react-router-dom';
+import { useAuthContext } from './AuthContext';
+
+const AuthOnly = ({ shouldNotBeAuthenticated = false, redirect, children }) => {
+  const { isSignedIn, isLoading } = useAuthContext();
+  if (isLoading) return null;
+  if (isSignedIn !== shouldNotBeAuthenticated) return children;
+  else if (redirect) return <Redirect to={redirect} />;
+  else return null;
+};
+
+export default AuthOnly;
