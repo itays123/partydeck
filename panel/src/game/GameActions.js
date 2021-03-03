a72dc1f4801798eb959479eb277928e750e42d3b
itays123 <itays2005@gmail.com>
2021-03-03 21:28:15 +0200

    FEATURE: Discard & Save buttons

--- panel/src/game/GameActions.js
+++ panel/src/game/GameActions.js
@@ -3,13 +3,14 @@ import EditorOnly from './edit/EditorOnly';
 import { useGameEditorContext } from './edit/GameEditorContext';
 
 const GameActions = () => {
-  const { play, remove } = useGameEditorContext();
+  const { isChanged, save, play, remove, discard } = useGameEditorContext();
   return (
     <section className="actions flex justify-start flex-row-reverse">
       <NewGamesOnly gameShouldExist>
         <button
-          className="play px-3 py-1 ml-4 text-purple-900 font-medium flex focus:outline-none"
+          className="play px-3 py-1 ml-4 text-purple-900 font-medium flex focus:outline-none disabled:opacity-50"
           onClick={play}
+          disabled={isChanged}
         >
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -43,6 +44,40 @@ const GameActions = () => {
             Remove
           </button>
         </NewGamesOnly>
+        <button
+          className="save px-3 py-1 ml-4 text-purple-900 font-medium flex focus:outline-none disabled:opacity-50"
+          onClick={save}
+          disabled={!isChanged}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="fill-current"
+            width={24}
+            height={24}
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z" />
+          </svg>
+          Save
+        </button>
+        <button
+          className="save px-3 py-1 ml-4 text-purple-900 font-medium flex focus:outline-none disabled:opacity-50"
+          onClick={discard}
+          disabled={!isChanged}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="fill-current"
+            width={24}
+            height={24}
+          >
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M19 6.41L17.59 5 12 10.59 6.41 5 5 6.41 10.59 12 5 17.59 6.41 19 12 13.41 17.59 19 19 17.59 13.41 12z" />
+          </svg>
+          Discard
+        </button>
       </EditorOnly>
     </section>
   );
