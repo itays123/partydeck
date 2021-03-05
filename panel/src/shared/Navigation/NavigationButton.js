e3d8ec2e7c62af3c08354a05a3ec787b7dc348f1
itays123 <itays2005@gmail.com>
2021-03-05 15:16:56 +0200

    FIX: Tiny design changes

--- panel/src/shared/Navigation/NavigationButton.js
+++ panel/src/shared/Navigation/NavigationButton.js
@@ -2,7 +2,7 @@ import { NavLink } from 'react-router-dom';
 
 const NavigationButton = ({ to = '/', children, external = false }) => {
   const className =
-    'flex items-center justify-center h-full px-3 hover:bg-gray-300 dark:hover:bg-purple-700 text-gray-700 dark:text-purple-300';
+    'flex items-center justify-center h-full px-3 hover:bg-gray-300 text-gray-700';
   return !!!external ? (
     <NavLink to={to} className={className}>
       {children}
