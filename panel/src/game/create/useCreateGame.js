c59280b2af02359609150b9fc5f1294d9bb1999a
itays123 <itays2005@gmail.com>
2021-03-07 16:21:08 +0200

    FEATURE: Simulate loading states in hooks

--- panel/src/game/create/useCreateGame.js
+++ panel/src/game/create/useCreateGame.js
@@ -1,10 +1,18 @@
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export function useCreateGame() {
   const history = useHistory();
+  const [isLoading, setLoading] = useState(false);
+
   return {
+    isLoading,
     create(game) {
-      history.push('/game/1');
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        history.push('/game/1');
+      }, 3000);
     },
   };
 }
--- panel/src/game/edit/useSaveGame.js
+++ panel/src/game/edit/useSaveGame.js
@@ -1,10 +1,17 @@
+import { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 
 export function useSaveGame() {
   const history = useHistory();
+  const [isLoading, setLoading] = useState(false);
   return {
+    isSaveLoading: isLoading,
     save(changes) {
-      history.push('/game/1');
+      setLoading(true);
+      setTimeout(() => {
+        setLoading(false);
+        history.push('/game/1');
+      }, 3000);
     },
   };
 }
