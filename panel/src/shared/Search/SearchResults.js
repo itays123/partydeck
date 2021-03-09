8f72695f42e05f37124b47d3edfb5c8621c86f95
itays123 <itays2005@gmail.com>
2021-03-09 15:57:34 +0200

    REFACTOR: Search results responsive + button color

--- panel/src/shared/Search/SearchResults.js
+++ panel/src/shared/Search/SearchResults.js
@@ -8,20 +8,23 @@ const SearchResults = () => {
   const { result, isLoading, loadMore } = useSearch(encodeURI(q));
   return (
     <div className="scrollable">
-      <div className="container mx-auto pt-8">
+      <div className="container mx-auto pt-8 px-8 md:px-0">
         {result.length > 0 && (
           <h1 className="text-2xl font-bold">Search Results</h1>
         )}
       </div>
       <GameList games={result} />
       {isLoading ? (
-        <div className="container flex mx-auto mt-4">
+        <div className="container flex mx-auto mt-4 px-8 md:px-0">
           <Spinner />
           Loading...
         </div>
       ) : (
-        <div className="container mx-auto my-4">
-          <button className="dark-button w-full" onClick={() => loadMore()}>
+        <div className="container mx-auto my-4 px-8 md:px-0">
+          <button
+            className="colorful-button bg-purple-600 hover:bg-purple-800 w-full"
+            onClick={() => loadMore()}
+          >
             More
           </button>
         </div>
