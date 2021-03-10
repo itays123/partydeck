436ce13dec165fd18b2ab32c0fa1281f815be295
itays123 <itays2005@gmail.com>
2021-03-10 16:17:08 +0200

    FEATURE: Design button

--- panel/src/shared/Navigation/SignedInLinks.js
+++ panel/src/shared/Navigation/SignedInLinks.js
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import AuthOnly from '../../auth/AuthOnly';
+import SvgWrapper from '../SvgWrapper';
 import NavigationButton from './NavigationButton';
 import SignedInDialog from './SignedInDialog';
 
@@ -34,13 +35,16 @@ const SignedInLinks = () => {
       <NavigationButton to="/create" hideOnSmallScreens>
         Create Game
       </NavigationButton>
-      <div className="relative">
+      <div className="relative flex items-center">
         <button
-          className="rounded h-10 w-10 active:outline-none focus:outline-none"
+          className="h-10 w-10 active:outline-none focus:outline-none"
           onClick={() => setDialogOpen(bool => !bool)}
           ref={buttonRef}
         >
-          Click me
+          <SvgWrapper className="text-gray-500 " w={32} h={32}>
+            <path d="M0 0h24v24H0z" fill="none" />
+            <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z" />
+          </SvgWrapper>
           {isDialogOpen && <SignedInDialog />}
         </button>
       </div>
