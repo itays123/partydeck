90d1035affcddcc50b55d2e809ac32223d223220
itays123 <itays2005@gmail.com>
2021-03-12 15:38:30 +0200

    REFACTOR: Remove unused imports

--- panel/src/auth/AuthContext.js
+++ panel/src/auth/AuthContext.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext } from 'react';
 import { useFetch } from '../shared/helpers/useAsyncFetch';
 
 const AuthContext = createContext();
