c130c80aa7fc0eb3bfaa3bc07e2bf579e32bf649
itays123 <itays2005@gmail.com>
2021-03-14 15:49:37 +0200

    FIX: Use judge data in rounds

--- game/src/round/Question.js
+++ game/src/round/Question.js
@@ -1,9 +1,16 @@
 import { useGameContext } from '../game/GameContext';
 
 const Question = () => {
-  const { question } = useGameContext();
+  const { question, judge, useMode } = useGameContext();
   return (
-    <h1 className="text-center text-gray-100 text-3xl mt-4">{question}</h1>
+    <>
+      <h1 className="text-center text-gray-100 text-3xl mt-4">{question}</h1>
+      {useMode && (
+        <h2 className="text-center text-gray-100 text-2xl mt-2">
+          {judge} is judging!
+        </h2>
+      )}
+    </>
   );
 };
 
