66b20912d1a1e10c9865310b3437c9e04d63c886
itays123 <itays2005@gmail.com>
2021-03-24 17:41:13 +0200

    FIX: Window open

--- panel/src/game/view/usePlayGame.js
+++ panel/src/game/view/usePlayGame.js
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { GameWebsiteLink } from '../../shared/helpers/GameWebsiteLink';
 import { useFetch } from '../../shared/helpers/useAsyncFetch';
 
@@ -8,16 +8,19 @@ export function usePlayGame(gameId) {
     'GET',
     false
   );
+  const windowRef = useRef();
 
   useEffect(() => {
     if (data.code && !isLoading) {
-      window.open(GameWebsiteLink + '?code=' + data.code);
+      const url = GameWebsiteLink + '?code=' + data.code;
+      windowRef.current.location = url;
     }
   }, [data, isLoading]);
 
   return {
     play() {
       console.log('playing...');
+      windowRef.current = window.open();
       doFetch();
     },
   };
