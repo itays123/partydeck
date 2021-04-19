21364cff0d7e764e26b357cbb7eda178599642bb
itays123 <itays2005@gmail.com>
2021-04-19 08:25:35 +0300

    INIT: Spring server

---
+++ server/src/test/java/com/partydeck/server/ServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partydeck.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
