194beea8885b212aa4a904ec6674ad56f277a179
itays123 <itays2005@gmail.com>
2021-04-19 14:46:22 +0300

    REFACTOR: Circle class docs

--- server/src/main/java/com/partydeck/server/models/Circle.java
+++ server/src/main/java/com/partydeck/server/models/Circle.java
@@ -12,7 +12,7 @@ import java.util.function.Consumer;
  */
 public class Circle<K,T extends Identifiable<K>> implements Iterable<T> {
 
-    private Queue<T> queue;
+    private final Queue<T> queue;
 
     /**
      * Default constructor - initializes the queue.
@@ -72,39 +72,58 @@ public class Circle<K,T extends Identifiable<K>> implements Iterable<T> {
         return Optional.of(entry);
     }
 
+
     /**
-     * Returns an iterable form of the circle
-     * @return the iterable of the circle
+     * Returns an iterator over elements of type {@code T}.
+     *
+     * @return an Iterator.
      */
-    @Override
     public Iterator<T> iterator() {
         return queue.iterator();
     }
 
     /**
-     * Loops through the entries
-     * @param action the action to perform on them
+     * Performs the given action for each element of the {@code Iterable}
+     * until all elements have been processed or the action throws an
+     * exception.  Actions are performed in the order of iteration, if that
+     * order is specified.  Exceptions thrown by the action are relayed to the
+     * caller.
+     * <p>
+     * The behavior of this method is unspecified if the action performs
+     * side-effects that modify the underlying source of elements, unless an
+     * overriding class has specified a concurrent modification policy.
+     *
+     * @param action The action to be performed for each element
+     * @throws NullPointerException if the specified action is null
+     * @implSpec <p>The default implementation behaves as if:
+     * <pre>{@code
+     *     for (T t : this)
+     *         action.accept(t);
+     * }</pre>
+     * @since 1.8
      */
-    @Override
     public void forEach(Consumer<? super T> action) {
         queue.forEach(action);
     }
 
     /**
-     * Returns a spliterator of the circle
-     * @return the spliterator of the circle.
+     * Creates a {@link Spliterator} over the elements described by this
+     * {@code Iterable}.
+     *
+     * @return a {@code Spliterator} over the elements described by this
+     * {@code Iterable}.
+     * @implSpec The default implementation creates an
+     * <em><a href="../util/Spliterator.html#binding">early-binding</a></em>
+     * spliterator from the iterable's {@code Iterator}.  The spliterator
+     * inherits the <em>fail-fast</em> properties of the iterable's iterator.
+     * @implNote The default implementation should usually be overridden.  The
+     * spliterator returned by the default implementation has poor splitting
+     * capabilities, is unsized, and does not report any spliterator
+     * characteristics. Implementing classes can nearly always provide a
+     * better implementation.
+     * @since 1.8
      */
-    @Override
     public Spliterator<T> spliterator() {
         return queue.spliterator();
     }
-
-    /**
-     * Checks the size of the circle
-     * @return the size in an integer form.
-     */
-    public int size() {
-        return queue.size();
-    }
-
 }
