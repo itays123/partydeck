ae5d36378c257b3bc5be0fae254e95dc5338ba16
itays123 <itays2005@gmail.com>
2021-04-19 15:05:56 +0300

    INIT: Card model

---
+++ server/src/main/java/com/partydeck/server/models/shared/Card.java
@@ -0,0 +1,34 @@
+package com.partydeck.server.models.shared;
+
+import com.partydeck.server.models.shared.Identifiable;
+
+/**
+ * An object representing a card in the game
+ * @author Itay Schechner
+ * @version 1.0
+ */
+public class Card implements Identifiable<String> {
+
+    private String id;
+
+    private String content;
+
+    public Card(String id, String content) {
+        this.id = id;
+        this.content = content;
+    }
+
+    @Override
+    public boolean is(String id) {
+        return this.id.equals(id);
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    public String getContent() {
+        return content;
+    }
+}
