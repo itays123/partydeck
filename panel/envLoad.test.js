817679eb53ec3cc3cb5c1cf19ca890fa41dc7344
itays123 <itays2005@gmail.com>
2021-05-13 11:23:19 +0300

    FIX: Load environment correctly

--- panel/envLoad.test.js
+++
@@ -1,8 +0,0 @@
-const envFilePath =
-  process.env.TARGET === 'cloud' ? '.env.server.prod' : '.env.server';
-
-const path = require('path');
-require('dotenv').config({ path: path.resolve(process.cwd(), envFilePath) });
-
-console.log(process.env.MONGO_URI);
-console.log(process.env.JWT_SECRET);
