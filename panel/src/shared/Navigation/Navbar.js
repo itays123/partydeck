daea185a71184c93129727d73713dfa1c5664927
itays123 <itays2005@gmail.com>
2021-05-14 18:08:31 +0300

    FIX: Make logo clickable

--- panel/src/shared/Navigation/Navbar.js
+++ panel/src/shared/Navigation/Navbar.js
@@ -4,15 +4,16 @@ import NavigationButton from './NavigationButton';
 import logo from '../../partydeck.svg';
 import SignedInLinks from './SignedInLinks';
 import SignedOutLinks from './SignedOutLinks';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <nav className="bg-gray-100 nav px-8 w-screen flex">
       <div className="md:px-4 lg:px-6 flex items-center h-16 md:ml-8">
         <img src={logo} width={20} height={20} alt="" />
-        <h1 className="text-lg md:text-xl text-purple-900 font-bold">
+        <Link to="/" className="text-lg md:text-xl text-purple-900 font-bold">
           Partydeck
-        </h1>
+        </Link>
       </div>
       <Search className="flex-grow py-3 hidden md:block" />
       <div className="links flex flex-grow md:flex-none items-stretch justify-end pl-4">
