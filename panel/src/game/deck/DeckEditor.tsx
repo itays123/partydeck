f1f6c7c08bf8280ef16defe281a92730d8690434
itays123 <itays2005@gmail.com>
2021-07-13 15:02:21 +0300

    FEATURE: Re-add the add card button

--- panel/src/game/deck/DeckEditor.tsx
+++ panel/src/game/deck/DeckEditor.tsx
@@ -1,4 +1,6 @@
+import SvgWrapper from '../../shared/SvgWrapper';
 import { Editor } from '../types';
+import EditorOnly from '../wrapper/EditorOnly';
 import EditableCard from './EditableCard/EditableCard';
 import { withEditor } from './withEditor';
 
@@ -14,6 +16,7 @@ function DeckEditor({ editor, label }: Props): JSX.Element {
     focusedCardIndex,
     setFocusedCardIndex,
     canDelete,
+    addCard,
   } = editor;
   return (
     <div className="card-list container mx-auto overflow-y-visible mt-4 px-8 md:px-2">
@@ -35,6 +38,17 @@ function DeckEditor({ editor, label }: Props): JSX.Element {
             onDeletePress={atom.remove}
           />
         ))}
+        <EditorOnly>
+          <button
+            className="rounded text-center py-8 border-4 border-dashed border-gray-500 flex items-center justify-center focus:outline-none"
+            onClick={() => addCard()}
+          >
+            <SvgWrapper w={48} h={48} className="text-gray-500">
+              <path d="M0 0h24v24H0z" fill="none" />
+              <path d="M19 13h-6v6h-2v-6H5v-2h6V5h2v6h6v2z" />
+            </SvgWrapper>
+          </button>
+        </EditorOnly>
       </div>
     </div>
   );
