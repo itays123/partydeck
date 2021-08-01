8b9be17b79dbdfb5bcb8954530d23d68ef55f1db
itays123 <itays2005@gmail.com>
2021-08-01 16:49:51 +0300

    INIT: Footer, footer buttons

--- panel/src/Home/Home.tsx
+++ panel/src/Home/Home.tsx
@@ -1,9 +1,13 @@
+import Footer from '../shared/Footer/Footer';
+import CardCollection from './CardCollection';
 import Header from './Header';
 
 const Home = () => {
   return (
     <div className="home scrollable">
       <Header />
+      <CardCollection />
+      <Footer />
     </div>
   );
 };
