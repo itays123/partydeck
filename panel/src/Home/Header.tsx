fd4ede466e3903e0e2c55d4d7908b8965fc6bc53
itays123 <itays2005@gmail.com>
2021-08-01 16:04:28 +0300

    FIX: Responsive header

--- panel/src/Home/Header.tsx
+++ panel/src/Home/Header.tsx
@@ -3,7 +3,7 @@ import Logo from '../resources/brand/Logo';
 export default function Header() {
   return (
     <header className="header bg-theme-600 flex justify-center font-roboto items-center space-x-4 p-4">
-      <Logo className="w-36 md:w-52 lg:w-full" />
+      <Logo className="w-36 md:w-52 lg:w-auto" />
       <div className="slogan flex flex-col justify-start items-between text-white text-xl md:text-2xl lg:text-3xl font-bold">
         <span>The</span>
         <span className="text-5xl md:text-7xl lg:text-9xl">BEST</span>
