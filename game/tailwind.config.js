48a15ec63a076384391a5e752489152199ac3c12
itays123 <itays2005@gmail.com>
2021-08-04 19:48:02 +0300

    INIT: Color theme + Fonts (live client)

--- game/tailwind.config.js
+++ game/tailwind.config.js
@@ -1,8 +1,42 @@
+const colors = require('tailwindcss/colors');
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
   purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false, // or 'media' or 'class'
+  darkMode: 'class', // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      theme: colors.indigo,
+      gray: colors.blueGray,
+      transparent: {
+        DEFAULT: '#00000000',
+      },
+      white: {
+        DEFAULT: '#F9FAFB',
+      },
+      pink: {
+        DEFAULT: '#EC4899',
+        light: '#F6C5DD',
+        dark: '#831843',
+      },
+      orange: {
+        DEFAULT: '#F59E0B',
+        light: '#F9DFB2',
+        dark: '#78350F',
+      },
+      green: {
+        DEFAULT: '#76E235',
+        light: '#D2F3BF',
+        dark: '#365314',
+      },
+    },
+    fontFamily: {
+      sans: ['"Rubik"', ...fontFamily.sans],
+      roboto: ['"Roboto"', ...fontFamily.sans],
+    },
+    fill: {
+      current: 'currentColor',
+    },
   },
   variants: {
     extend: {
