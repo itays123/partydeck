971c0924d9b9bd46ea919f9980fa4d5e2e94bb06
itays123 <itays2005@gmail.com>
2021-08-08 11:03:44 +0300

    REFACTOR: TypeScripted useSearch hook

--- panel/src/library/search/useSearch.ts
+++ panel/src/library/search/useSearch.ts
@@ -1,9 +1,12 @@
 import { useEffect, useState } from 'react';
+import { Game } from '../../components/GameList/types';
 import { useFetch } from '../../shared/helpers/useFetch';
 
-export function useSearch(query) {
-  const [result, setResult] = useState([]);
-  const { isLoading, doFetch, data } = useFetch('/search?q=' + query);
+export function useSearch(query: string) {
+  const [result, setResult] = useState<Game[]>([]);
+  const { isLoading, doFetch, data } = useFetch<any, { result: Game[] }>(
+    '/search?q=' + query
+  );
 
   useEffect(() => {
     if (data.result) {
