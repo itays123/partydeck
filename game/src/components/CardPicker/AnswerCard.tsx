8bc04bbea99bb08038727323f8785dea74807e48
itays123 <itays2005@gmail.com>
2021-08-10 21:29:37 +0300

    REFACTOR: Remove inner AnswerCard div

--- game/src/components/CardPicker/AnswerCard.tsx
+++ game/src/components/CardPicker/AnswerCard.tsx
@@ -9,14 +9,14 @@ export interface AnswerCardProps {
 export function AnswerCard({ position, content, innerRef }: AnswerCardProps) {
   return (
     <div
-      className={`rounded w-32 h-48 px-3 py-2 absolute inset-x-card-center answer-card-pos-${Math.abs(
+      className={`rounded w-32 h-48 px-3 py-8 absolute inset-x-card-center answer-card-pos-${Math.abs(
         position
       )}`}
       ref={innerRef as LegacyRef<HTMLDivElement>}
     >
-      <div className="h-full w-full flex flex-col justify-between items-center text-theme-800 font-medium py-8">
-        <p>{content}</p>
-      </div>
+      <p className="text-theme-800 font-medium mx-auto text-center">
+        {content}
+      </p>
     </div>
   );
 }
