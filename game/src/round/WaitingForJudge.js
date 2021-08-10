4f0bfe260bca8d7a972494d1917d14f00c17bc75
itays123 <itays2005@gmail.com>
2021-08-10 16:07:45 +0300

    REFACTOR: Delete unused files

--- game/src/round/WaitingForJudge.js
+++
@@ -1,9 +0,0 @@
-import { useCurrentRound } from '../game/GameContext';
-import Waiting from './Waiting';
-
-const WaitingForJudge = () => {
-  const { judge } = useCurrentRound();
-  return <Waiting text={`Waiting for ${judge}...`} />;
-};
-
-export default WaitingForJudge;
--- game/src/shared/types.ts
+++
@@ -1,2 +0,0 @@
-export type Wrapper = { children: JSX.Element | JSX.Element[] | string };
-export type ConditionalProps = Wrapper & { fallback: JSX.Element };
