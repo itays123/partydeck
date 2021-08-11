0de36c6d611cac12d5aca1aa5bc6e97b398f8015
itays123 <itays2005@gmail.com>
2021-08-12 02:45:59 +0300

    REFACTOR: Remove icon at EditableCard

--- panel/src/game/deck/EditableCard/EditableCard.tsx
+++ panel/src/game/deck/EditableCard/EditableCard.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { LegacyRef, MouseEventHandler, RefObject } from 'react';
 import { useFocusOnRender } from '../../../components/forms/useFocusOnRender';
-import SvgWrapper from '../../../shared/SvgWrapper';
+import Clear from '../../../components/icons/Clear';
 import { useGameEditorContext, EditorOnly } from '../../GameEditorContext';
 import './EditableCard.css';
 
@@ -56,13 +56,10 @@ const EditableCard = ({
         {canDelete && (
           <EditorOnly>
             <button
-              className="absolute -top-2 -right-2 opacity-0 group-hover:opacity-90 hover:opcaity-100 focus:outline-none"
+              className="absolute -top-2 -right-2 opacity-0 group-hover:opacity-90 hover:opcaity-100 focus:outline-none rounded-full bg-theme-600 p-0.5"
               onClick={onDeletePress}
             >
-              <SvgWrapper className="text-gray-500">
-                <path d="M0 0h24v24H0z" fill="none" />
-                <path d="M12 2C6.47 2 2 6.47 2 12s4.47 10 10 10 10-4.47 10-10S17.53 2 12 2zm5 13.59L15.59 17 12 13.41 8.41 17 7 15.59 10.59 12 7 8.41 8.41 7 12 10.59 15.59 7 17 8.41 13.41 12 17 15.59z" />
-              </SvgWrapper>
+              <Clear width={20} height={20} className="text-white" />
             </button>
           </EditorOnly>
         )}
